b35930e4efabea0fa6a8b12282562f4983be0483
Hope Jerry <15716862191@163.com>
2023-12-29 17:13:53 +0800

    正则表达式

--- src/views/compiler/regexper_cy/regexper_cy.vue
+++ src/views/compiler/regexper_cy/regexper_cy.vue
@@ -1,22 +1,58 @@
 <template>
     <div>
-        <el-card class="box-card">
+        <el-card class="box-card" v-for="item in zz">
             <template #header>
                 <div class="card-header">
-                    <span>常用正则</span>
+                    <span>{{ item.title }}</span>
                 </div>
             </template>
             <div>
-                <el-descriptions title="密码验证类" direction="vertical" :column="4" :size="size"
-                    border>
-                    
-                </el-descriptions>
+                <div class="demo-collapse">
+                    <el-collapse v-model="activeName" accordion>
+                        <el-collapse-item v-for="it in item.regexps" :name="it.name">
+                            <template #title>
+                                {{ it.title }}
+                            </template>
+                            <div>
+                                {{ it.content }}
+                            </div>
+                        </el-collapse-item>
+                    </el-collapse>
+                </div>
             </div>
         </el-card>
     </div>
 </template>
 
-<script>
+<script lang="ts" setup>
+import { ref } from 'vue'
+const activeName = ref('1')
+
+
+const zz = [
+    {
+        title: "密码验证类",
+        regexps: [
+            {
+                name: "1",
+                title: "6-16位字符，区分大小写（不能是9位以下的纯数字，不含空格）",
+                content: "^(?!\d{6,8}$)(?! )(?=.*[a-z])(?=.*[0-9])[a-zA-Z0-9_]{6,16}$"
+            },
+            {
+                name: "2",
+                title: "6-16位字符，区分大小写（不能是9位以下的纯数字，不含空格），必须包含大写字母",
+                content: "^(?!\d{6,8}$)(?! )(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[a-zA-Z0-9_]{6,16}$"
+            },
+            {
+                name: "3",
+                title: "密码不能为纯数字或字母，不少于6位",
+                content: "^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,}$"
+            }
+        ]
+    }
+]
+
+
 </script>
 
 <style></style>
